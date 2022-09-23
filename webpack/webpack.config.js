1e876fe83411a7dfdaca1953d049abf28d6a6bcd
MatteoGisondi <matteo.gisondi@gmail.com>
2022-09-23 10:17:21 -0400

    feat: :lipstick: Add html-webpack-plugin to render popup html onclick

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,29 +1,33 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack"); // to access built-in plugins
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
-   mode: "production",
-   entry: {
-      background: path.resolve(__dirname, "..", "src", "background.ts"),
-   },
-   output: {
-      path: path.join(__dirname, "../dist"),
-      filename: "[name].js",
-   },
-   resolve: {
-      extensions: [".ts", ".js"],
-   },
-   module: {
-      rules: [
-         {
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-            exclude: /node_modules/,
-         },
-      ],
-   },
-   plugins: [
-      new CopyPlugin({
-         patterns: [{from: ".", to: ".", context: "public"}]
-      }),
-   ],
+    mode: "production",
+    entry: {
+        background: path.resolve(__dirname, "..", "src", "background.ts"),
+    },
+    output: {
+        path: path.join(__dirname, "../dist"),
+        filename: "[name].js",
+    },
+    resolve: {
+        extensions: [".ts", ".js"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: ".", to: ".", context: "public" }],
+        }),
+        new HtmlWebpackPlugin({ template: "./src/index.html" }),
+    ],
 };
