281fcecfbc6883c424e725baf5f1f6f90f1c6c09
MatteoGisondi <matteo.gisondi@gmail.com>
2022-10-14 13:50:35 -0400

    Event stream progress

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,19 @@
-chrome.action.onClicked.addListener((tab) => {
-    console.log(tab);
+import { read } from "./api/stream";
+
+const options = {
+    method: "GET",
+    headers: { Authorization: "", Accept: "application/x-ndjson" },
+};
+
+chrome.action.onClicked.addListener(tab => {
+    if (!tab.active) {
+        return;
+    }
+
+    let stream = fetch(`https://lichess.org/api/stream/event`, options)
+
+    const onMessage = (obj: any) => console.log(obj);
+    const onComplete = () => console.log("The stream has completed");
+
+    stream.then(read(onMessage)).then(onComplete);
 });
