517b6eca7a42f89e9f548ec9fcd645d81b79eb3e
MatteoGisondi <matteo.gisondi@gmail.com>
2022-11-02 22:09:06 -0400

    test: :test_tube: WIP for automating extension_id

--- src/__tests__/popup.test.tsx
+++ src/__tests__/popup.test.tsx
@@ -1,40 +1,46 @@
-import puppeteer from 'puppeteer'
+import puppeteer from "puppeteer";
+
 const EXTENSION_PATH = "./dist";
-const EXTENSION_ID = "olacamfdpjchhfblnegjjpimpeckmhdb";
+const EXTENSION_ID = "";
 
+async function getExtensionId(browser: puppeteer.Browser): Promise<string | undefined> {
+    const targets = await browser.targets();
+    const extension_target = targets.find(target => {
+        target.type() == "service_worker";
+    });
+    if (extension_target) {
+        const url = extension_target.url() || "";
+        const [, , extension_id] = url.split("/");
+        return extension_id;
+    }
+}
 
-describe('Test Extension', () =>{
-    it("first test", async ()=>{
+describe("Test index.html", () => {
+    it("should check if elements are rendered correctly", async () => {
         const browser = await puppeteer.launch({
             headless: false,
-            args: [
-            `--disable-extensions-except=${EXTENSION_PATH}`,
-            `--load-extension=${EXTENSION_PATH}`
-            ],
-        })
+            args: [`--disable-extensions-except=${EXTENSION_PATH}`, `--load-extension=${EXTENSION_PATH}`],
+        });
         var [page] = await browser.pages();
+        // var extension_id = await getExtensionId(browser);
         await page.goto(`chrome-extension://${EXTENSION_ID}/index.html`);
+
         await page.waitForSelector("button#yes");
-        const textEl = await page.$("button#yes");
-        const text = await textEl?.evaluate((e) => e.innerHTML);
+        const textElemYes = await page.$("button#yes");
+        const yes = await textElemYes?.evaluate(e => e.innerHTML);
 
         await page.waitForSelector("button#no");
-        const textEl1 = await page.$("button#no");
-        const text1 = await textEl1?.evaluate((e) => e.innerHTML);
+        const textElemNo = await page.$("button#no");
+        const no = await textElemNo?.evaluate(e => e.innerHTML);
 
         await page.waitForSelector("h1");
-        const textEl2 = await page.$("h1");
-        const text2 = await textEl2?.evaluate((e) => e.innerText);
-        
-        console.log(text);
-        console.log(text1);
-        console.log(text2);
-        expect(text).toEqual("Yes, show me!");
-        expect(text1).toEqual("No thanks");
-        expect(text2).toEqual("Analyze Game?");
+        const textElemHeader = await page.$("h1");
+        const header = await textElemHeader?.evaluate(e => e.innerText);
+
+        expect(yes).toEqual("Yes, show me!");
+        expect(no).toEqual("No thanks");
+        expect(header).toEqual("Analyze Game?");
 
         browser.close();
     });
-
-    
-})
+});
