e195e18cb0b5866839365256fb0162b7980973e2
delispeter19 <48844162+delispeter19@users.noreply.github.com>
2023-01-09 16:49:49 -0500

    CB-62: Add safeguard and refactor chrome notif utils (#24)
    * Add safeguard and refactor chrome notif utils
    
    * fix: :twisted_rightwards_arrows: Fix merge conflict
    
    * fix: Fixed CI
    
    Co-authored-by: MatteoGisondi <matteo.gisondi@gmail.com>

--- src/chrome/utils.ts
+++ src/chrome/utils.ts
@@ -1,9 +1,3 @@
-type NotifConfig = {
-    iconUrl: string,
-    title: string,
-    message: string
-}
-
 export function download(url: string, filename: string): void {
     chrome.downloads.download({
         url: url,
@@ -11,8 +5,9 @@ export function download(url: string, filename: string): void {
     });
 }
 
-export function notification(notifConfig: NotifConfig): void {
-    const { iconUrl, title, message } = notifConfig;
+export function notification(message: string): void {
+    const iconUrl = "icons/icon48.png";
+    const title = "Chess Boom";
 
     chrome.notifications.create(
         {
@@ -24,4 +19,8 @@ export function notification(notifConfig: NotifConfig): void {
         },
         () => {}
     );
+}
+
+export function tabQuery(queryData: Object, callback: (args: any) => void ): void {
+    chrome.tabs.query(queryData, callback);
 }
